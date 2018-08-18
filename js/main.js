7e29b530e388c118105d4de89273b6a39772f72b
Ghada Farrag <ghain4ghada@gmail.com>
2018-08-18 17:01:24 +0200

    Apply offline version

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ let restaurants,
 var newMap
 var markers = []
 
+const indexController = new IndexController(document.querySelector('.main'));
+
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
@@ -78,7 +81,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiZ2hhaW40Z2hhZGEiLCJhIjoiY2praXB3anRrMTduNDNxczZxbjl6YjdmNiJ9.PLVq-xKKSOcDQ3CB497SeA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
